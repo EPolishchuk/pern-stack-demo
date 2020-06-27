8935b953adbfc40c9610c04adcc82ae73c93a879
Eldar Polishchuk <eldar.polishchuk@gmail.com>
2020-06-27 16:36:18 +0300

    PERN stack demo | add db conection settings

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
+require('dotenv').config({ path: '../.env.local' });
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const pool = require('./db');
+
 app.use(cors());
 app.use(express.json());
+
+/*
+  ROUTES
+*/
+
+//create
+
+//get all
+
+//get single
+
+//update
+
+//delete
 
 app.listen(5000, () => {
   console.log('server has started on port 5000');
