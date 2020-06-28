d2ed2c1e916cef0bd55e476709dee0c2b63d5cac
Eldar Polishchuk <eldar.polishchuk@gmail.com>
2020-06-28 14:03:00 +0300

    PERN stack demo | add React boilerplate

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const InputTodo = () => {
+  return (
+    <Fragment>
+      <h1 className='text-center mt-5'>Input Todo</h1>
+    </Fragment>
+  );
+};
+
+export default InputTodo;
