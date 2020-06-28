6328b1297939b18c85f0c7365cb602e163368461
Eldar Polishchuk <eldar.polishchuk@gmail.com>
2020-06-28 19:03:49 +0300

    PERN stack demo | React: add delete todo function

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -3,6 +3,18 @@ import React, { Fragment, useEffect, useState } from 'react';
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
 
+  const deleteTodo = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: 'DELETE',
+      });
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   const getTodos = async () => {
     try {
       const response = await fetch('http://localhost:5000/todos');
@@ -20,7 +32,6 @@ const ListTodos = () => {
 
   return (
     <Fragment>
-      <h1>List Todos</h1>
       <table class='table mt-5 text-center'>
         <thead>
           <tr>
@@ -31,10 +42,17 @@ const ListTodos = () => {
         </thead>
         <tbody>
           {todos.map((todo) => (
-            <tr>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className='btn btn-danger'
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
