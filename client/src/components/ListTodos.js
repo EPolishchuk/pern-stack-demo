a95b432b96c2983ac34e121214524c4c0a294fd7
Eldar Polishchuk <eldar.polishchuk@gmail.com>
2020-06-28 18:56:05 +0300

    PERN stack demo | React: create ListTodos component

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,46 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/todos');
+      const jsonData = await response.json();
+
+      setTodos(jsonData);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <Fragment>
+      <h1>List Todos</h1>
+      <table class='table mt-5 text-center'>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => (
+            <tr>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td>Delete</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
